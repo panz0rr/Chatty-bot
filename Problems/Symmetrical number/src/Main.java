fbc53136f82a87cfa08c955627f0ed49419051d7
AndreaAldana <65413878+AndreaAldana@users.noreply.github.com>
2020-05-21 16:24:54 -0400

    Otro ejercicio de if else usando boolean

--- Problems/Symmetrical number/src/Main.java
+++ Problems/Symmetrical number/src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
-    }
+        Scanner in = new Scanner(System.in);
+
+        String a = in.nextLine();
+        boolean b;
+        if ( b = a.charAt(3) == a.charAt(0) && a.charAt(1) == a.charAt(2)) {
+            System.out.println("1");
+    } else{
+            System.out.println("37");
+        }
+}
 }
