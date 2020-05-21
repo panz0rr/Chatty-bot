698138cb0ef970d2e7c051adb629c612773be00e
AndreaAldana <65413878+AndreaAldana@users.noreply.github.com>
2020-05-21 15:53:52 -0400

    Ejercicio de if else

---
+++ Problems/Check a number is positive/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+
+
+        if (a>0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
--- Problems/Exactly one parameter is positive/src/Main.java
+++ Problems/Exactly one parameter is positive/src/Main.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        int c=scanner.nextInt();
 
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
+        boolean caca = a> 0 && b<=0 && c<=0 || b> 0 && a<= 0 && c<= 0 || c>0 && b<=0 && a<=0;
 
-        boolean eq1 = a <= 0 && b <= 0 && c > 0;
-        boolean eq2 = a <= 0 && b > 0 && c <= 0;
-        boolean eq3 = a > 0 && b <= 0 && c <= 0;
+        System.out.println(caca);
 
-        System.out.println(eq1 || eq2 || eq3);
+
+
     }
 }
