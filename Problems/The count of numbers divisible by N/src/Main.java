9473f36b6abaaddc59c0f962479808b3186716e8
AndreaAldana <65413878+AndreaAldana@users.noreply.github.com>
2020-05-21 22:37:00 -0400

    Ejercicio de for if

--- Problems/The count of numbers divisible by N/src/Main.java
+++ Problems/The count of numbers divisible by N/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b =  in.nextInt();
+        int n = in.nextInt();
+        int contador = 0;
+
+        for (int i = a; i <= b; ++i) {
+            if (i % n == 0) {
+                contador = contador + 1;
+            }
+        }
+        System.out.println(contador);
     }
 }
