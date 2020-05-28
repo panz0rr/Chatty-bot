440f529281a21ef653a149146b652dc8959903a4
AndreaAldana <65413878+AndreaAldana@users.noreply.github.com>
2020-05-28 17:30:37 -0400

    ejercicio metodo y cast

---
+++ Problems/Composite numbers/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static boolean isComposite(long number) {
+        // write your code here
+    }
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final long number = scanner.nextLong();
+        System.out.println(isComposite(number));
+    }
+}
